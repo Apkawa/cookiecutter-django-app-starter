43c3f49bdfcdb08e667a77481f39fd9b8c3c337e
Arkadii Ivanov <apkawa@gmail.com>
2018-03-22 14:23:07 +0300

    fixes

--- {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/apps.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.app_name}}/apps.py
@@ -4,4 +4,4 @@ from django.utils.translation import ugettext_lazy as _
 
 class {{cookiecutter.app_config_name}}(BaseConfig):
     name = '{{cookiecutter.app_name}}'
-    verbose_name = _('{{cookiecutter.app_name.title()|replace("_", " "}}')
+    verbose_name = _('{{cookiecutter.app_name.title()|replace("_", " ")}}')
